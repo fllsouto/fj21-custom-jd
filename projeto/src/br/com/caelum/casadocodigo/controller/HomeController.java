43bef4c320f83d4e09979154a37df89e07aa3f1f
Fellipe Souto <fllsouto@gmail.com>
2020-07-26 20:17:46 -0300

    Adicionando mais configurações ao projeto

---
+++ projeto/src/br/com/caelum/casadocodigo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.caelum.casadocodigo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String execute() {
+		System.out.println("Executando a lógica com Spring MVC");
+		return "home";
+	}
+}
